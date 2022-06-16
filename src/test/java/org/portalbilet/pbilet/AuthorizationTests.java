94b230d655ec98fe0a961d7edcccfa013590ebf9
teddu0 <107142125+teddu0@users.noreply.github.com>
2022-06-16 11:29:49 +0300

    Обновил базовый класс

--- src/test/java/org/portalbilet/pbilet/AuthorizationTests.java
+++ src/test/java/org/portalbilet/pbilet/AuthorizationTests.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class AuthorizationTests extends Base {
+public class AuthorizationTests extends BaseSelenium {
 
     private final static String BASE_URL = "https://portalbilet.pbilet.org/en/login";
     private final static String EMAIL = "testeam@internet.ru";
--- src/test/java/org/portalbilet/pbilet/BaseSelenium.java
+++ src/test/java/org/portalbilet/pbilet/BaseSelenium.java
@@ -1,32 +1,30 @@
 package org.portalbilet.pbilet;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
-import java.lang.module.Configuration;
 import java.util.concurrent.TimeUnit;
 
 
-public class Base {
+public class BaseSelenium {
     public WebDriver driver;
 
-    public void setUp() {
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().setSize(new Dimension(1920, 1080));
-    }
-
     @BeforeTest
-    public void init() {
-        setUp();
-
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
     }
 
     @AfterTest
     public void tearDown() {
+        driver.close();
         driver.quit();
     }
 
