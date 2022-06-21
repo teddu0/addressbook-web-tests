ba41853f59d0cdc9fca7d54d593bdabfcea484e0
teddu0 <107142125+teddu0@users.noreply.github.com>
2022-06-21 20:52:21 +0300

    Перенес все методы в TestBase согласно урока

--- src/test/java/org/portalbilet/pbilet/BaseSeleniumTest.java
+++ src/test/java/org/portalbilet/pbilet/BaseSeleniumTest.java
@@ -3,13 +3,13 @@ package org.portalbilet.pbilet;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 import java.util.concurrent.TimeUnit;
-
-import static org.portalbilet.pbilet.BaseSeleniumPage.driver;
 
 public class BaseSeleniumTest {
     public WebDriver driver;
@@ -36,6 +36,42 @@ public class BaseSeleniumTest {
     public void tearDown() {
         driver.close();
         driver.quit();
+    }
+    //кнопка перехода к данным для выплат
+    public void setIndividualInfo() {
+        driver.findElement(By.xpath("//*[@id=\"__next\"]/div[7]/div/div/aside/ul/li[4]/button")).click();
+    }
+    // Поля формы для физ. лиц
+    public void enterParamsForIndividual(String name, String address, String bank_details, String phone_number, String email, String date_of_birth, String passport_data){
+        driver.findElement(By.name("name")).sendKeys(name);
+        driver.findElement(By.name("address")).sendKeys(address);
+        driver.findElement(By.name("bank_details")).sendKeys(bank_details);
+        driver.findElement(By.name("phone_number")).sendKeys(phone_number);
+        driver.findElement(By.name("email")).sendKeys(email);
+        driver.findElement(By.name("date_of_birth")).sendKeys(date_of_birth);
+        driver.findElement(By.name("passport_data")).sendKeys(passport_data);
+    }
+
+    // Поля формы для юр. лиц
+    public void enterParamsForLegals(String name, String registration_number, String address, String document_name, String bank_details, String phone_number, String email){
+        driver.findElement(By.name("name")).sendKeys(name);
+        driver.findElement(By.name("registration_number")).sendKeys(registration_number);
+        driver.findElement(By.name("address")).sendKeys(address);
+        driver.findElement(By.name("document_name")).sendKeys(document_name);
+        driver.findElement(By.name("bank_details")).sendKeys(bank_details);
+        driver.findElement(By.name("phone_number")).sendKeys(phone_number);
+        driver.findElement(By.name("email")).sendKeys(email);
+    }
+
+    // Кнопка сохрания формы и получение флажка уведомления о сохранении поля
+    public void submit() {
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        WebElement notice = driver.findElement(By.xpath("//div[@role='alert']"));
+        Assert.assertEquals(notice.getText(), "Data sent successfully!");
+    }
+
+    public void switchToLegalEntity () {
+        driver.findElement(By.xpath("//*[@id=\"__next\"]/div[7]/div/div/div/div/form/div[2]/label")).click();
     }
 
 }
--- src/test/java/org/portalbilet/pbilet/SetIndividualDataTest.java
+++ src/test/java/org/portalbilet/pbilet/SetIndividualDataTest.java
@@ -4,26 +4,27 @@ import org.testng.annotations.Test;
 
 public class SetIndividualDataTest extends BaseSeleniumTest {
 
-    UserPage u = new UserPage();
-    LoginPage l = new LoginPage();
-
     @Test
     public void setPaymentInfoForIndividualUser() throws InterruptedException {
         // Переход в раздел заполнения данныех для выплат
-        u.setIndividualInfo();
+        setIndividualInfo();
         // Заполнение данных для физ лица
-        u.enterParamsForIndividual("Anriano Smoke", "Iron Roads", "123", "3-34-45", "langress@hmail.com", "23.06.1992", "1221 321321");
-        // Сохранение формы
-        u.submit();
+        enterParamsForIndividual("Anriano Smoke", "Iron Roads", "123", "3-34-45", "langress@hmail.com", "23.06.1992", "1221 321321");
+        // Сохранение формы и сравнение результата
+        submit();
         Thread.sleep(4000);
     }
 
     @Test
     public void setPaymentInfoForLegalUser() throws InterruptedException {
-        u.setIndividualInfo();
-        u.switchToLegalEntity();
-        u.enterParamsForLegals("Enrique Huesis", "Iron Roads", "123", "3-34-45", "langress@hmail.com", "23.06.1992", "1221 321321");
-        u.submit();
+        // Переход в раздел заполнения данныех для выплат
+        setIndividualInfo();
+        // Переход к форме для юридических лиц
+        switchToLegalEntity();
+        // Заполнение данных для юр. лица
+        enterParamsForLegals("Enrique Huesis", "Iron Roads", "123", "3-34-45", "langress@hmail.com", "23.06.1992", "1221 321321");
+        // Сохранение формы и сравнение результата
+        submit();
         Thread.sleep(4000);
     }
 
